0f1900ea99fc3beaf61e5b68e9416a86e0fa7d42
Daniel Frederick <frederickd5@udayton.edu>
2026-02-19 20:16:01 -0500

    update

--- scripts/mysql_script.sh
+++ scripts/mysql_script.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/expect -f
 set timeout 30
-spawn mysql -u student -p -h 172.16.30.60
+spawn mysql -u student -p -h 172.16.30.60 --ssl=0
 expect "password:"
 send "student\r"
-expect "mysql>"
+expect "MySQL*>"
 send "show databases;\r"
-expect "mysql>"
+expect "MySQL*>"
 send "use tikiwiki;\r"
-expect "mysql>"
+expect "MySQL*>"
 send "show tables;\r"
-expect "mysql>"
+expect "MySQL*>"
 send "exit\r"
 expect eof
