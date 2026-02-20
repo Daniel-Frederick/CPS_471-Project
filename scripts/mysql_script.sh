c708d600a18a8300b3d9154ca65a054b4e9550eb
Daniel Frederick <frederickd5@udayton.edu>
2026-02-19 19:08:32 -0500

    update

--- scripts/mysql_script.sh
+++ scripts/mysql_script.sh
@@ -3,7 +3,7 @@ set timeout -1
 spawn $env(SHELL)
 match_max 100000
 
-send "mysql -u script_user -p -h 172.16.30.60\r"
+send "mysql -u student -p -h 172.16.30.60\r"
 
 expect "*password:"
 send "student\r" 
